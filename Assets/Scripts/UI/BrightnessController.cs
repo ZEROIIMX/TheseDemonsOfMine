b66fb841ec8219c79a23ba9533dd7efd6f62b234
Dahbobbbu <bch.johanna@gmail.com>
2025-11-10 16:33:44 +0100

    Accessibility options added to settings temp page

---
+++ Assets/Scripts/UI/BrightnessController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+using UnityEngine.UI;
+
+public class BrightnessController : MonoBehaviour
+{
+    [Header("References")]
+    public Volume globalVolume;      // Assign your Global Volume
+    public Slider brightnessSlider;  // Assign your UI Slider
+
+    private ColorAdjustments colorAdjustments;
+
+    private const float minExposure = -2f;
+    private const float maxExposure = 2f;
+
+    void Start()
+    {
+        // Get the ColorAdjustments override from the Volume
+        if (globalVolume.profile.TryGet(out colorAdjustments))
+        {
+            // Load saved brightness if available
+            float savedValue = PlayerPrefs.GetFloat("Brightness", 0.5f);
+            brightnessSlider.value = savedValue;
+
+            UpdateBrightness(savedValue);
+
+            // Connect slider to update function
+            brightnessSlider.onValueChanged.AddListener(UpdateBrightness);
+        }
+        else
+        {
+            Debug.LogError("ColorAdjustments not found in the Volume Profile!");
+        }
+
+    }
+
+    public void UpdateBrightness(float value)
+    {
+        if (colorAdjustments != null)
+        {
+            colorAdjustments.postExposure.value = Mathf.Lerp(minExposure, maxExposure, value);
+            PlayerPrefs.SetFloat("Brightness", value); // Save setting
+        }
+
+        PlayerPrefs.Save();
+    }
+}
