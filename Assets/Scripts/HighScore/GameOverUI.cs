2a8da84156c7d19fe1977d72739e81f1317c28e4
zax00000 <mateusz.swiderski123@gmail.com>
2025-11-12 16:19:13 +0100

    changes

--- Assets/Scripts/HighScore/GameOverUI.cs
+++ Assets/Scripts/HighScore/GameOverUI.cs
@@ -1,21 +1,57 @@
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class GameOverUI : MonoBehaviour
 {
+    [System.Serializable]
+    public class ScoreRowUI
+    {
+        public TextMeshProUGUI rankText;
+        public TextMeshProUGUI nameText;
+        public TextMeshProUGUI scoreText;
+        public GameObject rowObject;
+    }
+
     [Header("UI Elements")]
     [SerializeField] private TextMeshProUGUI finalResultText;
+    [SerializeField] private List<ScoreRowUI> scoreRows;
 
     void Start()
     {
         Cursor.visible = true;
         Cursor.lockState = CursorLockMode.None;
 
-        if (GameManager.Instance != null && finalResultText != null)
+        if (GameManager.Instance != null)
         {
-            string playerName = GameManager.Instance.GetLastName();
-            int finalScore = GameManager.Instance.GetLastScore();
-            finalResultText.text = $"{playerName}\nScore: {finalScore}";
+            if (finalResultText != null)
+            {
+                string playerName = GameManager.Instance.GetLastName();
+                int finalScore = GameManager.Instance.GetLastScore();
+                finalResultText.text = $"{playerName}\nScore: {finalScore}";
+            }
+
+            DisplayHighScores();
+        }
+    }
+
+    private void DisplayHighScores()
+    {
+        HighScores highScores = GameManager.Instance.GetHighScores();
+
+        for (int i = 0; i < scoreRows.Count; i++)
+        {
+            if (i < highScores.scores.Count)
+            {
+                scoreRows[i].rowObject.SetActive(true);
+                scoreRows[i].rankText.text = (i + 1).ToString();
+                scoreRows[i].nameText.text = highScores.scores[i].playerName;
+                scoreRows[i].scoreText.text = highScores.scores[i].score.ToString("D6");
+            }
+            else
+            {
+                scoreRows[i].rowObject.SetActive(false);
+            }
         }
     }
 
